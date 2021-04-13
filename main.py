7cdaf6144b3963aa2b63e297afa97d939389fa66
Haramrit Khurana <haramrit09k@gmail.com>
2021-04-12 21:39:49 -0400

    handled no-data error, added daily stock prices for selected company

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
 import yfinance as yf
 import streamlit as st
 import pandas as pd
+from datetime import date
 
+# concatenate names and symbols and store in another list
+# which will be used for populating the selection box
 tickers = pd.read_csv('tickers.csv')
 symbols = tickers['Symbol'].to_list()
 names = tickers['Company Name'].to_list()
 result = list(symbols[i]+' - '+names[i] for i in range(len(names)))
 
+# get today's date
+today = date.today()
+d1 = today.strftime("%A, %B %d, %Y") # YYYY-mm-dd
+
 st.write("""
 # Simple Stock Price App
-## {by haramrit singh khurana}
+## { by haramrit singh khurana }
 """)
 
 # tickerSymbol = "GOOGL"
 # tickerSymbol = st.text_input('Enter ticker')
-tickerSymbol = st.selectbox('Enter ticker', result)
+tickerSymbol = st.selectbox('Select company', result)
+
+tickerData = yf.Ticker(symbols[result.index(tickerSymbol)])
+
+tickerDf = tickerData.history(period='max')
+tickerDfToday = tickerData.history(period='1d')
+
+if tickerDfToday.empty:
+    df2 = {'Open': ['No data found'], 'Close': ['No data found'], 'High': ['No data found'], 'Low': ['No data found'], 'Volume': ['No data found'], 'Dividends': ['No data found']}
+    tickerDfToday = df2
+    
+st.text(tickerDfToday)
 
 st.write("""
-Shown are the stock closing price and volume of ** 
-"""  + names[result.index(tickerSymbol)] + "**: ")
+### """ + symbols[result.index(tickerSymbol)] + """
+#### """ + names[result.index(tickerSymbol)])
 
+# display today's prices
+st.write("""
+### Today (""",d1,"""):
+- Opening Price :
+""",tickerDfToday['Open'][0], """
+- Closing Price  :
+""",tickerDfToday['Close'][0], """
+- High  :
+""",tickerDfToday['High'][0] , """
+- Low  :
+""",tickerDfToday['Low'][0], """
+- Volume  :
+""",tickerDfToday['Volume'][0] , """
+- Dividends  :
+""",tickerDfToday['Dividends'][0])
+
+# display company details
 st.write("""
 ### Details:
 - Company Name :
@@ -33,13 +68,16 @@ st.write("""
 - Test Issue  :
 """,tickers['Test Issue'][result.index(tickerSymbol)] , """
 - Financial Status  :
-""",tickers['Financial Status'][result.index(tickerSymbol)], """
-- Round Lot Size  :
-""",tickers['Round Lot Size'][result.index(tickerSymbol)])
+""",tickers['Financial Status'][result.index(tickerSymbol)])
 
-tickerData = yf.Ticker(symbols[result.index(tickerSymbol)])
+st.line_chart(tickerDf.High)
+st.line_chart(tickerDf.Low)
 
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-
+st.line_chart(tickerDf.Open)
 st.line_chart(tickerDf.Close)
+
 st.line_chart(tickerDf.Volume)
+st.line_chart(tickerDf.Dividends)
+
+# st.line_chart(tickerDf.Stock)
+st.line_chart(tickerDf['Stock Splits'])
